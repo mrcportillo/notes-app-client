9ee3b71074f67f1308399dcf47419ec6f09c1a4a
marcos portillo <marcossportillo@gmail.com>
2020-05-21 17:59:13 -0300

    configured app

---
+++ src/config.js
@@ -0,0 +1,18 @@
+const REGION = "us-west-2";
+
+export default {
+  s3: {
+    REGION,
+    BUCKET: "mrc-serverless-notes-uploads",
+  },
+  apiGateway: {
+    REGION,
+    URL: "https://o8i1woxdjj.execute-api.us-west-2.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION,
+    USER_POOL_ID: "us-west-2_dtiQcPiZK",
+    APP_CLIENT_ID: "2rcr4v6cr9ri0gis9i3stjjjgc",
+    IDENTITY_POOL_ID: "us-west-2:d0fd8baa-1ff2-4232-8b6e-09f718eacbee",
+  },
+};
